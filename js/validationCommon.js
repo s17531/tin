7ae5c727059fbec6f351085a30ed28607a595aed
s17531 <s17531@pjwstk.edu.pl>
2020-12-16 21:46:40 +0100

    poprawki w formularzach edycji lektorów

--- js/validationCommon.js
+++ js/validationCommon.js
@@ -1,6 +1,7 @@
 function resetErrors(inputs, errorTexts, errorInfo) {
+
     for (let i = 0; i < inputs.length; i++) {
-        inputs[i].classList.remove("error_input");
+        inputs[i].classList.remove("error-input");
     }
     for (let i = 0; i < errorTexts.length; i++) {
         errorTexts[i].innerText = "";
--- js/validationTForm.js
+++ js/validationTForm.js
@@ -1,22 +1,16 @@
 function validateForm() {
-
     const firstNameInput = document.getElementById('firstName');
     const lastNameInput = document.getElementById('lastName');
-    const emailInput = document.getElementById('email');
-    const birthdateInput = document.getElementById('birthdate');
-
+    const languageInput = document.getElementById('language');
 
     const errorFirstName = document.getElementById('errorFirstName');
     const errorLastName = document.getElementById('errorLastName');
-    const errorEmail = document.getElementById('errorEmail');
-    const errorBirthdate = document.getElementById('errorBirthdate');
-
+    const errorLanguage = document.getElementById('errorLanguage');
     const errorsSummary = document.getElementById('errorsSummary');
 
-    resetErrors([firstNameInput, lastNameInput, emailInput], [errorFirstName, errorLastName, errorEmail], errorsSummary);
+    resetErrors([firstNameInput, lastNameInput, languageInput], [errorFirstName, errorLastName, errorLanguage], errorsSummary);
 
     let valid = true;
-
 
     if (!checkRequired(firstNameInput.value)) {
         valid = false;
@@ -38,29 +32,16 @@ function validateForm() {
         errorLastName.innerText = "Pole powinno zawierać od 2 do 60 znaków";
     }
 
-    if (!checkRequired(emailInput.value)) {
+
+    if (!checkRequired(languageInput.value)) {
         valid = false;
-        emailInput.classList.add("error-input");
-        errorEmail.innerText = "Pole jest wymagane";
-    } else if (!checkTextLengthRange(emailInput.value, 5, 60)) {
-        valid = false;
-        emailInput.classList.add("error-input");
-        errorEmail.innerText = "Pole powinno zawierać od 5 do 60 znaków";
-    } else if (!checkEmail(emailInput.value)) {
-        valid = false;
-        emailInput.classList.add("error-input");
-        errorEmail.innerText = "Pole powinno zawierać prawidłowy adres email";
+        languageInput.classList.add("error-input");
+        errorLanguage.innerText = "Pole jest wymagane";
     }
-    if (!checkRequired(birthdateInput.value)) {
-        valid = false;
-        birthdateInput.classList.add("error-input");
-        errorBirthdate.innerText = "Pole jest wymagane";
-    }
+
 
     if (!valid) {
         errorsSummary.innerText = "Formularz zawiera błędy";
     }
-
-
     return valid;
 }
